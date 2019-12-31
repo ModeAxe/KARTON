f15b8b6fec90374e1c7b3f34df7b6e1fd5f0fd24
ModeAxe <modeaxe@gmail.com>
2019-12-31 03:54:24 -0500

    Finished integration with maps and basic functionality

--- karton/lib/main.dart
+++ karton/lib/main.dart
@@ -1,5 +1,3 @@
-//REMEMBER TO ADD API KEY FOR FINAL BUILD
-
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'dart:async';
@@ -7,13 +5,12 @@ import 'dart:async';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'KARTON',
+      title: 'Flutter Google Maps',
       theme: ThemeData(
-        primarySwatch: Colors.amber,
+        primarySwatch: Colors.lightGreen,
       ),
       home: HomePage(),
     );
@@ -27,11 +24,20 @@ class HomePage extends StatefulWidget {
 
 class HomePageState extends State<HomePage> {
   Completer<GoogleMapController> _controller = Completer();
-
+  MapType type;
   static final CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
   );
+  Set<Marker> markers;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    type = MapType.hybrid;
+    markers = Set.from([]);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,20 +45,60 @@ class HomePageState extends State<HomePage> {
       body: Stack(
         children: <Widget>[
           GoogleMap(
-            mapType: MapType.hybrid,
+            markers: markers,
+            mapType: type,
+            onTap: (position){
+              Marker mk1 = Marker(
+                markerId: MarkerId('1'),
+                position: position,
+              );
+              setState(() {
+                markers.add(mk1);
+              });
+            },
             initialCameraPosition: _kGooglePlex,
             onMapCreated: (GoogleMapController controller) {
               _controller.complete(controller);
             },
           ),
-          FloatingActionButton(
-            onPressed: (){
-
-            },
-            child: Icon(Icons.map),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Row(
+              children: <Widget>[
+                FloatingActionButton(
+                  onPressed: () {
+                    setState(() {
+                      type = type == MapType.hybrid ? MapType.normal : MapType.hybrid;
+                    });
+                  },
+                  child: Icon(Icons.map),
+                ),
+                FloatingActionButton(
+                  child: Icon(Icons.zoom_in),
+                  onPressed: () async{
+                    (await _controller.future).animateCamera(CameraUpdate.zoomIn());
+                  },
+                ),
+                FloatingActionButton(
+                  child: Icon(Icons.zoom_out),
+                  onPressed: () async {
+                    (await _controller.future).animateCamera(CameraUpdate.zoomOut());
+                  },
+                ),
+                FloatingActionButton.extended(
+                  icon: Icon(Icons.location_on),
+                  label: Text("My position"),
+                  onPressed: (){
+                    if(markers.length < 1)
+                      print("no marker added");
+                    print(markers.first.position);
+                  },
+                )
+              ],
+            ),
           )
         ],
-      )
+      ),
     );
   }
 }
