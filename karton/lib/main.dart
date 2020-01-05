899bf6996102c2b74d3ca0dfe6869860e5b247d5
ModeAxe <modeaxe@gmail.com>
2020-01-05 01:07:20 -0500

    completed implementation/fixing error

--- karton/lib/main.dart
+++ karton/lib/main.dart
@@ -1,6 +1,26 @@
-// import 'package:flutter/material.dart';
-// import 'package:google_maps_flutter/google_maps_flutter.dart';
-// import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:karton/states/app_state.dart';
+import 'screens/home.dart';
 
-// void main() => runApp(MyApp());
+void main() {
+  return runApp(MultiProvider(providers: [
+      ChangeNotifierProvider.value(value: AppState(),)
+  ],
+  child: MyApp(),));
+}
 
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'KARTON',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(title: 'Karton'),
+    );
+  }
+}
